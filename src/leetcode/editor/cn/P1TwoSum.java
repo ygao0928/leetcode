a3add3151b26299fe62a33d2e85d439ffcfb92ed
Kevin <ygao0928@outlook.com>
2021-05-27 09:00:19 +0800

    算法2未完结

--- src/leetcode/editor/cn/P1TwoSum.java
+++ src/leetcode/editor/cn/P1TwoSum.java
@@ -61,10 +61,11 @@ public class P1TwoSum {
         public int[] twoSum(int[] nums, int target) {
             HashMap<Integer, Integer> map = new HashMap<>();
             for (int i = 0; i < nums.length; i++) {
-                if (map.containsKey(target-nums[i])){
-                    return new int[]{map.get(target-nums[i]),i};
+                int i1 = target - nums[i];
+                if (map.containsKey(i1)){
+                    return new int[]{map.get(i1),i};
                 }
-                map.put(target - nums[i],i);
+                map.put(nums[i],i);
             }
             return null;
         }
